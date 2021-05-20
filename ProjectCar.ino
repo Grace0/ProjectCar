983bc118ad4ae6627abcb471221acd9cece7e6a9
Grace Kwak <gracekwak25@gmail.com>
2021-05-19 20:58:48 -0700

    new sensor cal values

--- ProjectCar.ino
+++ ProjectCar.ino
@@ -1,9 +1,12 @@
 #include <ECE3.h>
 
+//encoders
+
 uint16_t sensorValues[8]; 
 
-uint16_t sensorMins[8] = {573, 504, 527, 573, 504, 550, 596, 596}; //may 6
-uint16_t sensorMaxes[8] = {1927, 1996,  1973,  1927,  1996,  1950,  1904,  1904};
+uint16_t sensorMins[8] = {600,  528, 552, 552, 460, 529, 575, 598};//{573, 504, 527, 573, 504, 550, 596, 596}; //may 6 -- get sensorMins each time?
+uint16_t sensorMaxes[8] = {1900, 1972,  1948,  1948,  2040,  1971,  1925,  1902};//{1927, 1996,  1973,  1927,  1996,  1950,  1904,  1904}; // get sensorMaxes each time?
+
 
 //const int DARKEST_READING = 1000;
 
@@ -24,9 +27,9 @@ const int LED_RB = 58; //right back - left front red
 const int LED_LB = 57; //left back - right front red
 
 //MOTOR CONSTANTS
-const float Kp = 0.0;
+const float Kp = 0.1; //250 error = 25 PWM change
 const float Kd = 0.0;
-const float Kf = 0.0;
+const float Kf = 11.0; // out of 255
 float P;
 float D;
 float F;
@@ -47,7 +50,7 @@ const int RUN = 1;
 const int DONUT = 2;
 const int FIND_PATH = 3;
 
-const int MAX_SPEED = 100;
+const float MAX_SPEED = 255;
 const float MAX_SPEED_INVERSE = 1 / MAX_SPEED;
 
 void setup() {
@@ -55,14 +58,14 @@ void setup() {
   pinMode(left_dir_pin,OUTPUT);
   pinMode(left_pwm_pin,OUTPUT);
 
-  digitalWrite(left_dir_pin,LOW);
+  digitalWrite(left_dir_pin,HIGH);
   digitalWrite(left_nslp_pin,HIGH);
 
   pinMode(right_nslp_pin,OUTPUT);
   pinMode(right_dir_pin,OUTPUT);
   pinMode(right_pwm_pin,OUTPUT);
 
-  digitalWrite(right_dir_pin,LOW);
+  digitalWrite(right_dir_pin, HIGH);
   digitalWrite(right_nslp_pin,HIGH);
 
   pinMode(sw_1_pin,INPUT);
@@ -73,7 +76,7 @@ void setup() {
   ECE3_Init();
 
   Serial.begin(9600); 
-  delay(2000); 
+  delay(2000); //like IR sensor example 
 
   calibrate();
 
@@ -84,45 +87,156 @@ void setup() {
 }
 
 void loop() {
-  Serial.print(carState);
-  switch (carState) {
-    case STOP:
-      stopMotors();
-      if (digitalRead(sw_1_pin)) {
-        carState = RUN;
-      }
-      break;
-    case RUN:
+
+  ECE3_read_IR(sensorValues);
+  calculateError();
+
+  P = Kp * error;
+  D = Kd * (error - prev_error);
+  F = Kf;
+
+  output = P + D;
+
+   lOutput = F;
+   rOutput = F;
+
+  lOutput += output;
+  rOutput -= output;
+
+//    if (lOutput > 0) { //...doesn't work?
+//    digitalWrite(left_dir_pin,HIGH);
+//  } else {
+//    digitalWrite(left_dir_pin,LOW);
+//  }
+//
+//    if (rOutput > 0) {
+//    digitalWrite(right_dir_pin,HIGH);
+//  } else {
+//    digitalWrite(right_dir_pin,LOW);
+//  }
+
+
+Serial.print(error); 
+Serial.println();
+  //Serial.print("\t");
+  //.Serial.println(rOutput);
+
+ // analogWrite(left_pwm_pin, lOutput);
+ // analogWrite(right_pwm_pin, rOutput);
+
+  //delay(100);
+        
+//  Serial.print("CAR STATE: ");
+//  Serial.print(carState);
+//  switch (carState) {
+//    case STOP:
+//      stopMotors();
+//      if (digitalRead(sw_1_pin)) {
+//        carState = RUN;
+//      }
+//      break;
+//    case RUN:
       // read raw sensor values
-        ECE3_read_IR(sensorValues);
-        runPID();
+//        ECE3_read_IR(sensorValues);
+//        calculateError();
+//       // Serial.println(error);
+//        runPID();
         // print the sensor values as numbers from 0 to 2500, where 0 means maximum reflectance and
         // 2500 means minimum reflectance
-        for (unsigned char i = 0; i < 8; i++)
-        {
-          Serial.print(sensorValues[i]);
-          Serial.print('\t'); // tab to format the raw data into columns in the Serial monitor
-        }
-        Serial.println();
+//        for (unsigned char i = 0; i < 8; i++)
+//        {
+//          Serial.print(sensorValues[i]);
+//          Serial.print('\t'); // tab to format the raw data into columns in the Serial monitor
+//        }
+//        Serial.println();
       
-        if (hasDetectedCrossbar()) {
-         // rotate180(true);
-        }
-      
-        if (hasLostPath()) {
-         // carState = STOP;
-        }
-        
-      break;
-    case DONUT:
-      break;
-    case FIND_PATH:
-      break;
-  }
+//        if (hasDetectedCrossbar()) {
+//         // rotate180(true);
+//        }
+//      
+//        if (hasLostPath()) {
+//         // carState = STOP;
+//        }
+//        
+//      break;
+//    case DONUT:
+//      break;
+//    case FIND_PATH:
+//      break;
+//  }
 
 
     
   }
+
+//sensor fusion
+void calculateError() {
+
+  for (unsigned char i = 0; i < 8; i++) {
+    sensorValues[i] -= sensorMins[i]; //644  573 596 620 549 573 573 596 -- mins
+
+    sensorValues[i] = sensorValues[i] / sensorMaxes[i] * 1000;
+  }
+  
+  error = sensorFusionAlg();
+}
+
+void runPID() {
+  calculateError();
+  //Serial.println(error);
+  P = Kp * error;
+  D = Kd * (error - prev_error);
+  F = Kf;
+
+//  if (abs(error) < errorThreshold) {
+//    return; //skip the rest of this and just don't do anything
+//  }
+
+  output = P + D;
+//
+  lOutput = output;
+  rOutput = output;
+
+  lOutput += F;
+  rOutput += F;
+//
+//  if (lOutput > MAX_SPEED) {
+//    lOutput = MAX_SPEED;
+//  }
+//
+//  if (rOutput > MAX_SPEED) {
+//    rOutput = MAX_SPEED;
+//  }
+//
+//  if (lOutput > 0) {
+//    digitalWrite(left_dir_pin,LOW);
+//  } else {
+//    digitalWrite(left_dir_pin,HIGH);
+//  }
+//
+//    if (rOutput > 0) {
+//    digitalWrite(right_dir_pin,LOW);
+//  } else {
+//    digitalWrite(right_dir_pin,HIGH);
+//  }
+
+  //Serial.print("L: ");
+ // Serial.print(lOutput);
+ //Serial.print('\t');
+ // Serial.print("R: ");
+  Serial.println(rOutput);
+  //analogWrite(left_pwm_pin, lOutput);
+  //analogWrite(right_pwm_pin, rOutput);
+
+//4 cases
+
+  prev_error = error;
+}
+
+float sensorFusionAlg() {
+  return (-8.0*sensorValues[0]-4.0*sensorValues[1]-2.0*sensorValues[2]-1.0*sensorValues[3]+1.0*sensorValues[4]+2.0*sensorValues[5]+4.0*sensorValues[6]+8.0*sensorValues[7])/4.0;
+}
+
 
 bool hasLostPath() { //if all sensors read <500, we lost the path
    for (unsigned char i = 0; i < 8; i++) {
@@ -176,63 +290,4 @@ void stopMotors() {
   analogWrite(left_pwm_pin,0);
   analogWrite(right_pwm_pin,0);
   delay(1000);
-}
-
-//sensor fusion
-void calculateError() {
-
-  for (unsigned char i = 0; i < 8; i++) {
-    sensorValues[i] -= sensorMins[i];
-    sensorValues[i] = sensorValues[i] / sensorMaxes[i] * 1000;
-  }
-
-  error = sensorFusionAlg();
-}
-
-void runPID() {
-  calculateError();
-  Serial.println(error);
-  P = Kp * error;
-  D = Kd * (error - prev_error);
-  F = Kf * (MAX_SPEED_INVERSE);
-
-  if (abs(error) < errorThreshold) {
-    return; //skip the rest of this and just don't do anything
-  }
-
-  output = P + D + F;
-
-  lOutput += output;
-  rOutput -= output;
-
-  if (lOutput > MAX_SPEED) {
-    lOutput = MAX_SPEED;
-  }
-
-  if (rOutput > MAX_SPEED) {
-    rOutput = MAX_SPEED;
-  }
-
-  if (lOutput > 0) {
-    digitalWrite(left_dir_pin,LOW);
-  } else {
-    digitalWrite(left_dir_pin,HIGH);
-  }
-
-    if (rOutput > 0) {
-    digitalWrite(right_dir_pin,LOW);
-  } else {
-    digitalWrite(right_dir_pin,HIGH);
-  }
-
-  analogWrite(left_pwm_pin, lOutput);
-  analogWrite(right_pwm_pin, rOutput);
-
-//4 cases
-
-  prev_error = error;
-}
-
-float sensorFusionAlg() {
-  return (-8*sensorValues[0]-4*sensorValues[1]-2*sensorValues[2]-1*sensorValues[3]+1*sensorValues[4]+2*sensorValues[5]+4*sensorValues[6]+8*sensorValues[7])/4.0;
 }
